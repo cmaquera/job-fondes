eafbeac8ec77e3b2cc374c9082476833110f49ec
Cesar Maquera <cmaquerat@gmail.com>
2023-11-21 12:34:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_dotenv()
 HOSTNAME = os.getenv('FTP_HOSTNAME')
 USERNAME = os.getenv('FTP_USERNAME')
 PASSWORD = os.getenv('FTP_PASSWORD')
-ANIO = '2022'
+ANIO = '2023'
 
 print('{} {} {}'.format(HOSTNAME, USERNAME, PASSWORD))
 
